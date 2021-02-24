86df0721364df7f06a0a496302be6f7cc4dfab43
Raphael Rabadan <raphael.rabadan@dataprev.gov.br>
2021-02-24 01:07:23 -0300

    Códigos realizados até a aula 10

---
+++ fundamentos/primeiro/primeiro.go
@@ -0,0 +1,14 @@
+// Programas executáveis iniciam pelo pacote main
+package main
+
+/*
+ Os códigos em Go são organizados em pacotes
+ Para usá-los é necessário declarar um ou vários imports
+*/
+import "fmt"
+
+// Porta de entrada de um pacoet main.
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Print("Programa!")
+}
